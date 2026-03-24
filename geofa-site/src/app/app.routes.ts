5819a302c3414d209e04b8ef1f0c794bc3f2efad
DESKTOP-5FC4RIC\HP <ardelonbalenda@gmail.com>
2026-03-24 22:34:18 +0100

    travaux du 24-03-2026

--- geofa-site/src/app/app.routes.ts
+++ geofa-site/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ObjectivesComponent } from './pages/objectives/objectives.component';
+import { MembersComponent } from './pages/members/members.component';
+import { OrganizationComponent } from './pages/organization/organization.component';
+import { DocumentsComponent } from './pages/documents/documents.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'a-propos', component: AboutComponent },
+  { path: 'objectifs', component: ObjectivesComponent },
+  { path: 'membres', component: MembersComponent },
+  { path: 'organisation', component: OrganizationComponent },
+  { path: 'documents', component: DocumentsComponent },
+  { path: 'contact', component: ContactComponent },
+];
